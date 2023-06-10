2d7c9e6a489869c1b77c9641ff766d0f30e898f9
BurhanDundar <burhandundar2399@gmail.com>
2023-06-10 20:35:07 +0300

    .

--- mobile/src/routes/favourite/post.ts
+++ mobile/src/routes/favourite/post.ts
@@ -1,17 +1,34 @@
 // Tamamlanacak
 
 import express from 'express';
-import { updateFav } from '../../models/favourites';
+import { getMemberFavAsUserIds, updateFav } from '../../models/favourites';
 import { ObjectId } from 'mongodb';
 
 
 export const _updateFav = async (req,res) => {
     let fav = {
-        "user_id": new ObjectId(req.body.userId),
-        "fav_id": new ObjectId(req.body.favId),
+        "user_id": new ObjectId(req.body.user_id),
+        "fav_id": new ObjectId(req.body.fav_id),
+        "fav_type": req.body.fav_type
     }
     let data = await updateFav(fav)
     if(data){res.send(data)} else {console.log("data yok")}
  }
 
-
+ 
+ export const _getMemberFavAsUserIds = async (req,res) => {
+    let obj = {
+        "user_id": new ObjectId(req.body.user_id),
+        "fav_type": req.body.fav_type
+    }
+    let data = await getMemberFavAsUserIds(obj.user_id, obj.fav_type);
+    // if(data){res.send(data)} else {console.log("data yok")}
+    if(data){
+        if(data[0]){
+            res.send(data[0].favs)
+        }
+    } else {
+        console.log("data yok")
+    }
+ }
+ 
